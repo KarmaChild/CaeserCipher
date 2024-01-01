41a47e86cb999727c7a4d270435ebd50c117952a
Adithya Mattappily <adithya1bijukumar@gmail.com>
2024-01-01 08:55:09 -0600

    Initial commit

---
+++ cipher.go
@@ -0,0 +1,16 @@
+package CaeserCipher
+
+type Cipher struct {
+}
+
+func (c Cipher) randomRounds() int8 {
+	// TODO
+}
+
+func (c Cipher) encrypt(text string, rounds int8) string {
+	// TODO
+}
+
+func (c Cipher) decrypt(text string, rounds int8) string {
+	// TODO
+}
